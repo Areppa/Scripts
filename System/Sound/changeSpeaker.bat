dcd10d2d5ca7d57fc848d63fbee3019c60b9f2d7
Aaro Halme <aaro.halme2000@gmail.com>
2024-09-30 06:30:36 +0300

    Fixed typo

--- System/Sound/changeSpeaker.bat
+++ System/Sound/changeSpeaker.bat
@@ -1,6 +1,6 @@
 @echo off
 
-rem This requires nicmd to be installed
+rem This requires nircmd to be installed
 rem Rename "Headset" to the name of your sound output
 
 nircmd setdefaultsounddevice "Headset" 1
